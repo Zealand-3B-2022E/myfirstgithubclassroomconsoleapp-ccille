560cbe6c3d0240b275ef4356ae801bcf03790bea
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-09-05 12:42:06 +0200

    Tilføjet property

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        private int _id;
+        private string _name;
+        private string _phone;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        public Demo():this(0, "", "")
+        {
+        }
+
+        public Demo(int id, string name, string phone)
+        {
+            _id = id;
+            _name = name;
+            _phone = phone;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {_id}, Name = {_name}, Phone = {_phone}";
+        }
+
+
+    }
+}
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyFirstGitHubClassRoomConsoleApp60;
+
 Console.WriteLine("Hello, World!");
+
+Demo de = new Demo(1, "Cecilie", "2093411");
+Console.WriteLine(de);
